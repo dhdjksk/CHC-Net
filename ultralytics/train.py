e564cef53da4dbc0a5d7def927bebe29349a6e06
yue-2002 <3298664126@qq.com>
2026-05-19 10:33:01 +0800

    rz

--- ultralytics/train.py
+++ ultralytics/train.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     model = YOLO('cfg/models/11/yolo11l.yaml')
     model.train(data=r'cfg/datasets/VisDrone.yaml',
                 imgsz=1024,
-                epochs=300,
+                epochs=300, 
                 batch=2,
                 project='runs',
                 name='VisDrone',
